895858ae2288b55805c23cca50beb2648af73dff
unknown <regmianish533@gmail.com>
2024-03-14 08:53:57 +0545

    test

--- main.py
+++ main.py
@@ -8,4 +8,23 @@ data[1].pop(2)
 print(data)
 data[0].insert(1,200)
 data[1].insert(2,500)
+print(data[0])
+data=[34,"ram","shyam"]
+print(data[2])
+data.append('ramlal')
 print(data)
+num=[10,20,30]
+num[0]=200
+print(num) 
+a="200"
+b=int(a)
+print(a)
+num=(100,254,544)  
+p=input("enter p:")
+t=input("enter t:")
+r=input("enter r:")
+p=int(p)
+t=int(t)
+r=int(r)
+print((p*t*r)/100)
+
